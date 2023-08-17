d256362a9c8848cdc661566ab58d743496dcc6c0
lin <1085828641@qq.com>
2023-08-17 20:44:36 +1000

    0817

---
+++ daily_practice/Greet.java
@@ -0,0 +1,11 @@
+package daily_practice;
+
+import java.util.Scanner;
+
+public class Greet {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str=in.nextLine();
+        System.out.println("Hi "+str+"!");
+    }
+}
---
+++ daily_practice/Greet.md
@@ -0,0 +1,3 @@
+create a class Greet that reads in from the console a string, and then outputs
+a greeting Hi ...!, where the name replaces the elipses. e.g. If the input
+is Hugh then your output should be Hi Hugh!.
